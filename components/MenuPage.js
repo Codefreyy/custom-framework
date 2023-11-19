eefdf482c5309ae57e3328bdbca863ad5460bbb2
godfrey <joyyujiepeng@gmail.com>
2023-11-19 10:24:31 +0000

    feat: apply a shadow dom

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,13 +1,18 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super()
+    // if you want to outside to access your inner dom or not
+    // this.root = this.attachShadow({ mode: "open" })
+    this.attachShadow({ mode: "open" })
   }
 
+  // connectCallback is a lifecycle method, it is runned when element was insert into the document tree
   connectedCallback() {
     const template = document.getElementById("menu-page-template")
     const content = template.content.cloneNode(true)
     // this指向
-    this.appendChild(content)
+    // this.root.appendChild(content)
+    this.shadowRoot.appendChild(content)
   }
 }
 
--- services/Router.js
+++ services/Router.js
@@ -5,32 +5,29 @@ const Router = {
       a.addEventListener("click", (event) => {
         event.preventDefault()
         const url = event.target.getAttribute("href")
-        /**
-         * Router.go() is used to navigate to specific route
-         */
+        // Router.go() is used to navigate to specific route
         Router.go(url)
       })
     })
     // Event handler for url changes (such as the use click baco or forward arrow)
-    // window.addEventListener("popstate", (event) => {
-    //   console.log("state", event.state)
-    //   Router.go(event.state.route, false)
-    // })
+    window.addEventListener("popstate", (event) => {
+      console.log("state", event.state)
+      Router.go(event.state.route, false)
+    })
     // Check the inital URL (if the user use a deep link at first)
     Router.go(location.pathname)
   },
   go: (route, addToHisotry = true) => {
     console.log(`Going to ${route}`)
     if (addToHisotry) {
-      /**
-       * history.pushState() is used to add new state to browser history
-       * and change current url
-       */
+      // history.pushState() is used to add new state to browser history and change current url
       history.pushState({ route }, "", route)
     }
+
     let pageElement = null
     switch (route) {
       case "/":
+        console.log(1, route)
         pageElement = document.createElement("menu-page")
         break
       case "/order":
@@ -41,12 +38,14 @@ const Router = {
           pageElement = document.createElement("details-page")
           const paramId = route.substring(route.lastIndexOf("-") + 1) // substring [a,b)
           pageElement.dataset.id = paramId
+        } else {
+          pageElement.textContent = "404 - Page not found"
         }
+        break
     }
     if (pageElement) {
-      const cache = document.querySelector("main")
-      cache.innerHTML = ""
-      cache.appendChild(pageElement)
+      document.querySelector("main").innerHTML = ""
+      document.querySelector("main").appendChild(pageElement)
       window.scrollX = 0
       window.scrollY = 0
     }
